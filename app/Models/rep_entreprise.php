8ce006506d505e811966fac25a0322d15d657d69
Mohamed Benazza <mohamed.benazza@etixway.com>
2023-10-02 11:55:25 +0200

    [AJOUT] Création des models

---
+++ app/Models/rep_entreprise.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class rep_entreprise extends Model{
+    use HasFactory;
+    protected $fillable = ['role'];
+}
---
+++ app/Models/salle.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class salle extends Model{
+    use HasFactory;
+    protected $fillable = ['nom', 'nb_place', 'disponibilite'];
+}
