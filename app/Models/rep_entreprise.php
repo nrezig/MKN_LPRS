fb7d88129dd4fa40ef3720e1fc43e229b0e3ece3
Mohamed Benazza <mohamed.benazza@etixway.com>
2023-10-04 16:59:51 +0200

    [AJOUT] Création du model admin et mise en heraitage des models utilisateurs

--- app/Models/rep_entreprise.php
+++ app/Models/rep_entreprise.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class rep_entreprise extends Model{
+class rep_entreprise extends User {
     use HasFactory;
     protected $fillable = ['role'];
 }
