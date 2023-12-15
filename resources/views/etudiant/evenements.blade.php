76f6deae6d1d5c04f5e490a3489ebf22a15fa08b
khatirsedjai <sedjaikhatir93@gmail.com>
2023-12-15 16:30:17 +0100

    [MODIF] vue evenement pour voir qui est l'organisateur

--- resources/views/etudiant/evenements.blade.php
+++ resources/views/etudiant/evenements.blade.php
@@ -7,7 +7,6 @@
         <a href="{{ route('etudiant.mesEvenements') }}" class="btn btn-secondary mb-3">Mes événements</a>
         <a href="{{ route('evenement.create') }}" class="btn btn-primary mb-3">Créer un Événement</a>
 
-
         @php
             $evenements = \App\Models\Evenement::where('valide', true)->get();
         @endphp
@@ -25,9 +24,13 @@
         @endif
 
         @foreach($evenements as $evenement)
+            @php
+                $organisateur = \App\Models\User::find($evenement->ref_users);
+            @endphp
             <div class="card mb-3">
                 <div class="card-body">
-                    <h3 class="card-title">{{ $evenement->nom }}</h3>
+                    <h3 class="card-title" style="font-size: 24px; font-weight: bold;">{{ $evenement->nom }}</h3>
+                    <p class="card-text">Organisé par : <span style="font-size: 12px;">{{ $organisateur->prenom }} {{ $organisateur->nom }}</span></p>
                     <p class="card-text">{{ $evenement->description }}</p>
                     <a href="{{ route('etudiant.detailEvenement', ['id' => $evenement->id]) }}" class="btn btn-primary">Voir les détails</a>
                 </div>
